f2a4a7a3084fd3986f95b418444b16f1655f70e4
mohassan <vanmowha@gmail.com>
2019-03-24 16:45:10 +0300

    add date

--- src/app/date-count.pipe.spec.ts
+++ src/app/date-count.pipe.spec.ts
@@ -2,7 +2,7 @@ import { DateCountPipe } from './date-count.pipe';
 
 describe('DateCountPipe', () => {
   it('create an instance', () => {
-    const pipe = new DateCountPipe();
+    co nst pipe = new DateCountPipe();
     expect(pipe).toBeTruthy();
   });
 });
