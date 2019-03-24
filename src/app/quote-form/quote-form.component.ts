d9fbe31d1f12d55c3ff8292b8b838de2b910c529
mohassan <vanmowha@gmail.com>
2019-03-24 18:32:32 +0300

    correction in quote-form-component ts

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, OutPut, EventEmitter } from '@angular/core';
-import { Quote } from '../quote'
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote-form',
@@ -9,6 +9,7 @@ import { Quote } from '../quote'
 export class QuoteFormComponent implements OnInit {
   newQuote = new Quote(0, "", "", "", new Date());
   @Output() addQuote = new EventEmitter<Quote>();
+
   submitQuote() {
     this.addQuote.emit(this.newQuote);
     this.newQuote = new Quote(0, "", "", "", new Date());
