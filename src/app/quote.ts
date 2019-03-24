7c8b6ea21304ade19dbcba5d4b0f86a2e4424344
mohassan <vanmowha@gmail.com>
2019-03-24 17:06:07 +0300

    add constructors in  quotes.ts

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,6 @@
 export class Quote {
+  public showDescription: boolean;
+  constructor(public id: number, public quote: string, public author: string, public publisher: string, public completeDate: Date) {
+    this.showDescription = false
+  }
 }
